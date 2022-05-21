a52f0dcda99bb6786c9e41f4e817b61b92329843
jaypatel9425 <105956530+jaypatel9425@users.noreply.github.com>
2022-05-21 18:23:00 -0400

    Add files via upload

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,21 @@
+function readTextFile(file)
+// {
+//     var XMLHttpRequest = require('xhr2');
+
+//     var rawFile = new XMLHttpRequest();
+//     rawFile.open("GET", file, false);
+//     rawFile.onreadystatechange = function ()
+//     {
+//         if(rawFile.readyState === 4)
+//         {
+//             if(rawFile.status === 200 || rawFile.status == 0)
+//             {
+//                 var allText = rawFile.responseText;
+//                 alert(allText);
+//             }
+//         }
+//     }
+//     rawFile.send(null);
+// }
+
+// readTextFile("file:///C:\Users\pjay9\OneDrive\Desktop\Sem 2\Web application\Practice project\read.txt");
